6da5dd5f79061ccf8fb9e2cb82c5d74d9ce95a89
Aayushi Jain <101443827+aayushi-jain-5@users.noreply.github.com>
2023-01-19 11:38:25 +0530

    Add files via upload

---
+++ loop.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+	int i,j;
+	char arr[7][10];
+	printf("Enter the week days");
+	for(i=0;i<7;i++){
+		scanf("%s",&arr[i]);
+	}
+	for(j=0;j<7;j+=2){
+		printf("%s",arr[j]);
+	}
+	
+	
+	return 0;
+}
